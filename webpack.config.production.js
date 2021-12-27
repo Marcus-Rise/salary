42db2e933489aaca490096a384c17b73c3efd98f
Marcus-Rise <ilykonst95@gmail.com>
2021-12-27 10:35:14 +0400

    ci: webpack development

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -2,15 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].[contenthash].js",
         clean: true,
-    },
-    devServer: {
-        static: "./dist",
     },
     plugins: [
         new HtmlWebpackPlugin({
