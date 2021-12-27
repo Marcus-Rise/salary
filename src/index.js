4ea9b3c3673ceae649acf379b900fd08566221f5
Marcus-Rise <ilykonst95@gmail.com>
2021-12-27 10:53:30 +0400

    refactor: extract function

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
+import {priceToString} from './price-to-string';
+
 const form = document.getElementById("form");
 const display = document.getElementById("data");
 
-const PRICE_STRING_DIVIDER = 3;
-const PRICE_STRING_TAIL_SIZE = 2;
 const NALOG = 0.87;
-
-/**
- * @param price {number}
- * @return string
- */
-const priceToString = (price) => {
-    const [head, tail] = price.toFixed(PRICE_STRING_TAIL_SIZE).toString().split(".");
-
-    if (head.length > PRICE_STRING_DIVIDER) {
-        let buf = "";
-        let i = head.length - PRICE_STRING_DIVIDER
-
-        for (; i >= 0; i = i - PRICE_STRING_DIVIDER) {
-            buf = head.substr(i, PRICE_STRING_DIVIDER) + " " + buf;
-        }
-
-        if (i < 0) {
-            buf = head.substr(0, i + PRICE_STRING_DIVIDER) + " " + buf;
-        }
-
-        buf = buf.trim();
-
-        if (Number(tail) === 0) {
-            return buf;
-        }
-
-        return `${buf}.${tail}`;
-    }
-
-    return price.toFixed(PRICE_STRING_TAIL_SIZE);
-};
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
