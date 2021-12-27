edff06bf2b0a3bd5749a4b677b97196fd236e80f
Ilya Konstantinov <ilykonst95@gmail.com>
2021-12-27 12:03:51 +0400

    Tests (#3)
    * test: first test
    
    * fix
    
    * test
    
    * test
    
    * style
    
    * test
    
    * style

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,16 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].[contenthash].js",
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Salary",
-            template: "public/index.html"
-        }),
-    ]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Salary",
+      template: "public/index.html"
+    }),
+  ],
 }
