f855a47e638bd5d4a7ed9db1ed268035bd95c193
Marcus-Rise <ilykonst95@gmail.com>
2021-12-27 11:03:21 +0400

    refactor: extract function

--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ const display = document.getElementById("data");
 
 const NALOG = 0.87;
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const data = new FormData(form);
-    const salaryGross = Number(data.get("salary_gross"));
-    const avansSalaryPercent = Number(data.get("avans_percent")) / 100;
-    const workingDaysFirstMonthHalf = Number(data.get("working_days_first_month_half"));
-    const workedDaysFirstMonthHalf = Number(data.get("worked_days_first_month_half"));
-    const workingDaysSecondMonthHalf = Number(data.get("working_days_second_month_half"));
-    const workedDaysSecondMonthHalf = Number(data.get("worked_days_second_month_half"));
-
+/**
+ *
+ * @param workedDaysFirstMonthHalf {number}
+ * @param workingDaysFirstMonthHalf {number}
+ * @param salaryGross {number}
+ * @param avansSalaryPercent {number}
+ * @param workedDaysSecondMonthHalf {number}
+ * @param workingDaysSecondMonthHalf {number}
+ * @return {{total: number, salaryPercent: number, avansPercent: number, salary: number, totalPercent: number, avans: number}}
+ */
+const calculateSalary = (
+    workedDaysFirstMonthHalf,
+    workingDaysFirstMonthHalf,
+    salaryGross,
+    avansSalaryPercent,
+    workedDaysSecondMonthHalf,
+    workingDaysSecondMonthHalf,
+) => {
     const avansGap = workedDaysFirstMonthHalf / workingDaysFirstMonthHalf;
     const avansPercent = Math.ceil(avansGap * 100);
     const avans = salaryGross * avansSalaryPercent * avansGap;
@@ -27,6 +34,35 @@ form.addEventListener("submit", (e) => {
 
     const total = salary + avans;
     const totalPercent = Math.ceil(total / (salaryGross * NALOG) * 100);
+    return {avansPercent, avans, salaryPercent, salary, total, totalPercent};
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+    const salaryGross = Number(data.get("salary_gross"));
+    const avansSalaryPercent = Number(data.get("avans_percent")) / 100;
+    const workingDaysFirstMonthHalf = Number(data.get("working_days_first_month_half"));
+    const workedDaysFirstMonthHalf = Number(data.get("worked_days_first_month_half"));
+    const workingDaysSecondMonthHalf = Number(data.get("working_days_second_month_half"));
+    const workedDaysSecondMonthHalf = Number(data.get("worked_days_second_month_half"));
+
+    const {
+        avansPercent,
+        avans,
+        salaryPercent,
+        salary,
+        total,
+        totalPercent
+    } = calculateSalary(
+        workedDaysFirstMonthHalf,
+        workingDaysFirstMonthHalf,
+        salaryGross,
+        avansSalaryPercent,
+        workedDaysSecondMonthHalf,
+        workingDaysSecondMonthHalf,
+    );
 
     display.innerHTML = `
         <p>К выплате: <strong>${priceToString(total)}</strong> (${totalPercent} %)</p>
