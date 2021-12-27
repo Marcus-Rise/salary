e30c1e34cf3f8a0db10c1d0790892b7416296a27
Marcus-Rise <ilykonst95@gmail.com>
2021-12-27 11:05:19 +0400

    refactor: extract function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
 import {priceToString} from './price-to-string';
+import {calculateSalary} from "./calculate-salary";
 
 const form = document.getElementById("form");
 const display = document.getElementById("data");
-
-const NALOG = 0.87;
-
-/**
- *
- * @param workedDaysFirstMonthHalf {number}
- * @param workingDaysFirstMonthHalf {number}
- * @param salaryGross {number}
- * @param avansSalaryPercent {number}
- * @param workedDaysSecondMonthHalf {number}
- * @param workingDaysSecondMonthHalf {number}
- * @return {{total: number, salaryPercent: number, avansPercent: number, salary: number, totalPercent: number, avans: number}}
- */
-const calculateSalary = (
-    workedDaysFirstMonthHalf,
-    workingDaysFirstMonthHalf,
-    salaryGross,
-    avansSalaryPercent,
-    workedDaysSecondMonthHalf,
-    workingDaysSecondMonthHalf,
-) => {
-    const avansGap = workedDaysFirstMonthHalf / workingDaysFirstMonthHalf;
-    const avansPercent = Math.ceil(avansGap * 100);
-    const avans = salaryGross * avansSalaryPercent * avansGap;
-
-    const salaryGap = (workedDaysSecondMonthHalf + workedDaysFirstMonthHalf)
-        / (workingDaysSecondMonthHalf + workingDaysFirstMonthHalf);
-    const salaryPercent = Math.ceil(salaryGap * 100);
-    const salary = salaryGross * salaryGap * NALOG - avans;
-
-    const total = salary + avans;
-    const totalPercent = Math.ceil(total / (salaryGross * NALOG) * 100);
-    return {avansPercent, avans, salaryPercent, salary, total, totalPercent};
-};
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
