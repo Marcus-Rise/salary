e38e920d25d2ac65c3cba545461eb83a04118345
Marcus-Rise <ilykonst95@gmail.com>
2022-02-12 15:17:49 +0400

    fix: webpack eslint

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -34,7 +34,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
     }),
-    new ESLintPlugin()
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    })
   ],
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
